197e99cafaea1e693503cd158d9b57f4bfd518e6
kundandualcube <kundan@dualcube.com>
2022-12-05 20:49:17 +0530

    .

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2022041900;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 20220120500;        // Requires this Moodle version.
+$plugin->requires  = 2022120500;        // Requires this Moodle version.
 $plugin->component = 'gradereport_quizanalytics'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.6 (Build: 2022111500)';
